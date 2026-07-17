d46e901fc2407c39332af946910beba6a475c560
meet <codingmeet26@gmail.com>
2026-07-17 12:47:43 +0530

    Migrate project build configuration to IntelliJ convention plugins, buildSrc, and libs.versions.toml version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.20" apply false
-    kotlin("plugin.serialization") version "2.0.20" apply false
+    kotlin("jvm") apply false
+    alias(libs.plugins.kotlinPluginSerialization) apply false
 }
 
 allprojects {
@@ -14,7 +14,7 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "buildsrc.convention.kotlin-jvm")
 
     dependencies {
         // Kotest testing framework
@@ -25,12 +25,7 @@ subprojects {
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
             freeCompilerArgs.add("-Xopt-in=kotlin.RequiresOptIn")
         }
-    }
-
-    tasks.withType<Test>().configureEach {
-        useJUnitPlatform()
     }
 }
